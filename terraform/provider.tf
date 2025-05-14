42038cea8d5b5371f9d0285dca04725e2f002297
Kledson Basso <kledsonhugo@gmail.com>
2025-05-14 12:13:26 -0300

    fix: update Terraform provider version and configuration

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,21 +1,20 @@
 # PROVIDER
 terraform {
 
-  required_version = "~> 1.8.4"
+  required_version = "~> 1.11.4"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.50"
+      version = "~> 5.97.0"
     }
   }
-
 
 }
 
 provider "aws" {
   region                   = "us-east-1"
-  shared_config_files      = ["./.aws/config"]
-  shared_credentials_files = ["./.aws/credentials"]
-  profile                  = "fiap-iac"
+  shared_config_files      = [".aws/config"]
+  shared_credentials_files = [".aws/credentials"]
+  profile                  = "iac"
 }
