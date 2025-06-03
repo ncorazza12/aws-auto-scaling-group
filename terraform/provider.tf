38b87e2b22e1654356f03c7fbaa4ad055e59501c
Kledson Basso <kledsonhugo@gmail.com>
2025-06-03 10:26:59 -0300

    fix: update required Terraform version and AWS provider version

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,12 +1,12 @@
 # PROVIDER
 terraform {
 
-  required_version = "~> 1.11.4"
+  required_version = "~> 1.12.1"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.97.0"
+      version = "~> 5.99.1"
     }
   }
 
